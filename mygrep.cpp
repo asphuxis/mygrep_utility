13a458da587f0fff35d201d11604b7d3548f35c5
asphuxis <jaakko.hautala+github@gmail.com>
2026-03-04 20:04:25 +0200

    Added some error and input checking as well as fixed some logic

--- mygrep.cpp
+++ mygrep.cpp
@@ -2,21 +2,34 @@
 #include <string>
 #include <fstream>
 
-
 using namespace std;
 
 void noarg(){
 
     string line;
-    cout << "Enter string from which to search for: ";
-    cin >> line;
+    while (line.empty()==true) {
+        cout << "Give string from which to search for: ";
+        getline(cin, line);
+        if (line.empty()==true)
+        {
+            cout << "You entered nothing. Please enter your query string or exit by pressing CTRL+C" << endl;
+        }
+        
+    }
     
     string pattern;
-    cout << "Enter search string: ";
-    cin >> pattern;
+    while (pattern.empty()==true) {
+        cout << "Give search string: ";
+        getline(cin, pattern);
+        if (pattern.empty()==true)
+        {
+            cout << "You entered nothing. Please enter your search string or exit by pressing CTRL+C" << endl;
+        }
+        
+    }
 
     if (line.find(pattern) != string::npos) {
-        cout << pattern << " found in " << line;
+        cout << pattern << " found in " << line << " at position " << line.find(pattern);
     } else {
         cout << pattern << " NOT found in " << line;
     }
@@ -25,34 +38,36 @@ void noarg(){
 
 int main(int argc, char *argv[]){
 
-    if (argc < 3) {
+    if (argc == 1) {
         noarg();
         return(0);
     }
-    
-    string pattern = argv[1]; // Haluttu teksti tai merkki jota haetaan
-    string fileLocation = argv[2]; // Tiedosto tai sijainti jota luetaan
-    string line;
 
-    ifstream file;
-    file.open(fileLocation);
-    istream* in = &file;
+    if (argc == 3) {
+        string pattern = argv[1]; // Haluttu teksti tai merkki jota haetaan
+        string fileLocation = argv[2]; // Tiedosto tai sijainti jota luetaan
+        string line;
 
-    int matchCount;
+        ifstream file;
+        file.open(fileLocation);
+        istream* in = &file;
 
-    while (getline(*in, line))
-    {
-        
-        if (line.find(pattern) != string::npos) {
-            std::cout << line << '\n';
-            matchCount++;
+        int matchCount;
+
+        while (getline(*in, line)) {
+            if (line.find(pattern) != string::npos) {
+                std::cout << line << '\n';
+                matchCount++;
+            }
+            
         }
-        
-    }
 
-    if (matchCount == 0)
-    {
-        cout << pattern << " was NOT found in " << fileLocation;
+        if (matchCount == 0) {
+            cout << pattern << " was NOT found in " << fileLocation;
+        }
+    } else {
+        cerr << "Incorrect usage - either input no arguments or input two [2] arguments.";
+        return(1);
     }
     
     return(0);
