6a114edfb86f83e20c04613d0eefb065e7c0f3e3
asphuxis <jaakko.hautala+github@gmail.com>
2026-03-05 18:43:45 +0200

    Changed logic for running different versions of the program

--- mygrep.cpp
+++ mygrep.cpp
@@ -7,25 +7,23 @@ using namespace std;
 void noarg(){
 
     string line;
-    while (line.empty()==true) {
+    while (line.empty()) {
         cout << "Give string from which to search for: ";
         getline(cin, line);
         if (line.empty()==true)
         {
             cout << "You entered nothing. Please enter your query string or exit by pressing CTRL+C" << endl;
-        }
-        
+        } 
     }
     
     string pattern;
-    while (pattern.empty()==true) {
+    while (pattern.empty()) {
         cout << "Give search string: ";
         getline(cin, pattern);
         if (pattern.empty()==true)
         {
             cout << "You entered nothing. Please enter your search string or exit by pressing CTRL+C" << endl;
         }
-        
     }
 
     if (line.find(pattern) != string::npos) {
@@ -38,12 +36,14 @@ void noarg(){
 
 int main(int argc, char *argv[]){
 
-    if (argc == 1) {
+    switch (argc) {
+    case 1:
+    {
         noarg();
         return(0);
-    }
-
-    if (argc == 3) {
+    }   
+    case 3:
+    {
         string pattern = argv[1]; // Haluttu teksti tai merkki jota haetaan
         string fileLocation = argv[2]; // Tiedosto tai sijainti jota luetaan
         string line;
@@ -52,22 +52,26 @@ int main(int argc, char *argv[]){
         file.open(fileLocation);
         istream* in = &file;
 
-        int matchCount;
-
+        int matchCount = 0;
         while (getline(*in, line)) {
             if (line.find(pattern) != string::npos) {
                 std::cout << line << '\n';
                 matchCount++;
             }
-            
         }
 
         if (matchCount == 0) {
             cout << pattern << " was NOT found in " << fileLocation;
         }
-    } else {
+
+        return(0);
+    }
+    default:
+    {
         cerr << "Incorrect usage - either input no arguments or input two [2] arguments.";
         return(1);
+    }
+    
     }
     
     return(0);
